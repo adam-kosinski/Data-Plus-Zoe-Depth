07c655ae394fb07cbeec2582e2aab33b5e604e8e
Adam Kosinski <amk119@duke.edu>
2023-06-23 13:45:52 -0400

    Added pretrained resource support for custom run, hopefully it works

--- custom_run.py
+++ custom_run.py
@@ -4,14 +4,24 @@ import argparse
 import os
 from PIL import Image
 from zoedepth.utils.misc import colorize
+from zoedepth.utils.config import get_config
+from zoedepth.models.builder import build_model
 
 # get input dir
 parser = argparse.ArgumentParser()
 parser.add_argument('input_dir')
+parser.add_argument("-p", "--pretrained_resource", type=str,
+                        required=False, default=None, help="Pretrained resource to use for fetching weights. If not set, default resource from model config is used,  Refer models.model_io.load_state_from_resource for more details.")
 args = parser.parse_args()
 
 # Zoe_NK
-model_zoe_nk = torch.hub.load(".", "ZoeD_NK", source="local", pretrained=True, config_mode="eval")
+if args.pretrained_resource:
+    overwrite = {"pretrained_resource": args.pretrained_resource}
+    config = get_config("zoedepth_nk", "infer", None, **overwrite)
+    model_zoe_nk = build_model(config)
+else:
+    model_zoe_nk = torch.hub.load(".", "ZoeD_NK", source="local", pretrained=True, config_mode="eval")
+
 
 ##### prediction
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
